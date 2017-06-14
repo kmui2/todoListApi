bcac1af6f2bfe1f6dc393f67609e1d363f23326a
Kevin Mui <kmui2@wisc.edu>
2017-06-13 19:33:53 -0500

    for ip address mongo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express'),
   bodyParser = require('body-parser');
   
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
+mongoose.connect('mongodb://104.131.24.33:27017/firstDB'); 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
